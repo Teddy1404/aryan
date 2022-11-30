a2ad84e537bd7bb071a6a36be6354e3817e5a60d
Aryan singh chandel <aryansinghchandel1404@gmail.com>
2022-11-30 21:40:07 +0530

    first commit

---
+++ codechef.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, x,y,X,Y;
+        cin>>n>>x>>y>>X>>Y;
+        int a = min(x,n-x+1);
+        int b = min(y,n-y+1);
+        int c = min(X,n-X+1);
+        int d = min(Y,n-Y+1);
+
+        cout<<min(abs(x-X)+abs(y-Y),min(c,d)+min(a,b))<<endl;
+    }
+}
